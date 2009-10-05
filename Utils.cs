1660de2eebee340765d7433168c16f859b72e6d9
Diego Elio 'Flameeyes' Pettenò <flameeyes@gmail.com>
2009-10-05 16:48:58 +0200

    Implement a StreamCopy function to copy data between two Stream objects.

--- Utils.cs
+++ Utils.cs
@@ -1,6 +1,6 @@
 
 using System;
-using System.Reflection;
+using System.IO;
 
 namespace Portability
 {
@@ -15,11 +15,33 @@ namespace Portability
 			_company = (AssemblyCompanyAttribute)Attribute.GetCustomAttribute(_base_assembly, typeof(AssemblyCompanyAttribute));
 			_product = (AssemblyProductAttribute)Attribute.GetCustomAttribute(_base_assembly, typeof(AssemblyProductAttribute));
 		}
-		
+
 		public static string CompanyName
 		{ get { return _company.Company; } }
 
 		public static string ProductName
 		{ get { return _product.Product; } }
+
+		/* Copy streams in 4MiB increments; we can safely assume that anything
+		 * running Mono *has* 4MiB of free memory at any point in time. */
+		private static int StreamCopyBlockSize = 4 * 1024 * 1024;
+
+		public static void StreamCopy(Stream input, Stream output)
+		{
+			int copied = 0;
+			/* We cannot use unknown-length this way :( */
+			int input_length = input.Length;
+
+			while(input.CanRead)
+			{
+				int tocopy = (input_length > (4096 * 1024)) ? 4096 * 1024 : input_length;
+				byte[] tmpbuff = new byte[tocopy];
+				int read = input.Read(tmpbuff, copied, tocopy);
+				output.Write(tmpbuff, copied, read);
+
+				copied += read;
+				input_length -= read;
+			}
+		}
 	}
 }
