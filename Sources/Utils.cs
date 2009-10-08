079949807621005f96ad33bdca4e403070e75159
Diego Elio 'Flameeyes' Pettenò <flameeyes@gmail.com>
2009-10-08 12:16:21 +0200

    Since the 4MiB size for the array will make Mono overflow, copy the stream in 4KiB increments and forget about stream size entirely.

--- Sources/Utils.cs
+++ Sources/Utils.cs
@@ -23,40 +23,17 @@ namespace Portability
 		public static string ProductName
 		{ get { return _product.Product; } }
 
-		/* Copy streams in 4MiB increments; we can safely assume that anything
-		 * running Mono *has* 4MiB of free memory at any point in time. */
-		private static int StreamCopyBlockSize = 4 * 1024 * 1024;
-
 		public static void StreamCopy(Stream input, Stream output)
-		{
-			int length = -1;
-
-			try {
-				length = (int)input.Length;
-			} catch (NotSupportedException) {
-				/* If we came here it means that we have no way
-				 * to get the input length */
-			}
-
-			StreamCopy(input, output, length);
-		}
-
-		public static void StreamCopy(Stream input, Stream output, int input_length)
 		{
 			int copied = 0;
 
 			while(input.CanRead)
 			{
-				int tocopy = (input_length > StreamCopyBlockSize) ? StreamCopyBlockSize : input_length;
-				byte[] tmpbuff = new byte[tocopy];
-				int read = input.Read(tmpbuff, copied, tocopy);
+				byte[] tmpbuff = new byte[4096];
+				int read = input.Read(tmpbuff, copied, 4096);
 				output.Write(tmpbuff, copied, read);
 
 				copied += read;
-
-				/* We don't have to care when input_length == -1. */
-				if ( input_length != -1 )
-					input_length -= read;
 			}
 		}
 	}
